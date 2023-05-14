0e3a2a4a254c252ba095bc2a3feefc1c677a2dc3
mithereal <mithereal@gmail.com>
2023-05-13 19:30:32 -0700

    .

--- config/config.exs
+++ config/config.exs
@@ -2,19 +2,5 @@ import Config
 
 config :ueberauth, Ueberauth,
   providers: [
-    simple: {Support.SimpleCallback, []},
-    redirector: {Support.Redirector, []},
-    with_request_path: {Support.Redirector, [request_path: "/login"]},
-    with_callback_path: {Support.SimpleCallback, [callback_path: "/login_callback"]},
-    using_default_options: {Support.DefaultOptions, []},
-    using_custom_options: {Support.DefaultOptions, [the_uid: "custom uid"]},
-    with_errors: {Support.WithErrors, []},
-    post_callback: {Support.SimpleCallback, [callback_methods: ["POST"]]},
-    post_callback_and_same_request_path:
-      {Support.SimpleCallback,
-       [
-         callback_methods: ["POST"],
-         request_path: "/auth/post_callback_and_same_request_path",
-         callback_path: "/auth/post_callback_and_same_request_path"
-       ]}
+    oauth2_shift4shop: {Shift4Shop.Strategy.Ueberauth, [request_path: "/auth/shift4shop", callback_path: "/auth/shift4shop/callback"]},
   ]
--- lib/oauth2_shift4shop.ex
+++ lib/oauth2_shift4shop.ex
@@ -2,6 +2,6 @@ defmodule Shift4Shop.Oauth2 do
   @moduledoc false
 
   def json_library() do
-    Jason
+    Application.get_env(:oauth2_shift4shop, :json_library, Jason)
   end
 end
